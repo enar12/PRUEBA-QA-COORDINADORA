72a143ea6c3f5c5cca60fed42eac61f9c27fee52
Enar Daza <enardaza@gmail.com>
2025-02-08 18:19:00 -0500

    Api test

---
+++ tests/ApiTest.spec.ts
@@ -0,0 +1,18 @@
+import { expect, test } from '@playwright/test';
+
+test("API Guia coordinadora", async ({ playwright }) => {
+  let apiContext = await playwright.request.newContext({});
+  //Nos regresa la infomración del API
+  const apiInfo= await apiContext.get("99020276264");
+  //Se verifica el resultado
+  expect (apiInfo.ok()).toBeTruthy();
+  // Se captura el JSON de respuesta
+  const body = await apiInfo.json();
+
+  expect (body.data).toHaveProperty("valorRecaudar",38500);
+  expect (body.data).toHaveProperty("referenciaRecaudo","Prueba QA coordinadora");
+
+
+});
+
+
